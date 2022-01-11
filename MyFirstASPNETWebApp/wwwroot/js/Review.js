f3a89edad8faef8558c0b1fa8a767c49bec2d4ca
Spencer Walker <swalker2172@gmail.com>
2022-01-11 00:00:18 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/Review.js
@@ -0,0 +1,8 @@
+﻿$("#btnSend").click( function () {
+    alert("Review Sent by: " + $("#txtName").val() +
+        ", with " + $("#txtStars").val() +
+        " stars given. \n " +
+        "Explanation: " + $("#txtWhy").val());
+    $("#picGame").fadeToggle();
+})
+
